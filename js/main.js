2c1e4f7c4617589ca19cb4e1cf255c9663556288
Sarah Besozzi <sarah.besozzi@gmail.com>
2015-10-07 16:41:38 -0400

    Added HTML skeleton layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 (function () {
 
-  console.log('It Works!');
+// var firstItem = etsy.results[0];
 
+var templateString = $('#itemTemplate').text();
+
+// var templateString = '
+// // <div>
+// // <p>
+// // <%= title %>
+// // </p>
+// // <p class+"price">$<%<%= price %>
+// // </p>
+// // </div>';
+
+
+// Call template function to call out string 
+var templateFunction = _.template(templateString);
+
+_.each(etsy.results, function (item) {
+  
+  var itemHTML = templateFunction(item);
+  
+  $('.container').append(itemHTML);
+
+});
+
+  
 }());
